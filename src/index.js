8923a6f285e77226d4aa186b46e7cd0208aad787
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-08-09 19:49:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
