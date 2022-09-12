b8d169d30135fcf9bca4fc95e39dddf38fa6c24b
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-12 17:29:45 +0200

    Update Contentreviews.js

--- src/components/Contentreviews.js
+++ src/components/Contentreviews.js
@@ -3,18 +3,18 @@ import React from "react";
 function Contentreviews(){
     return(
         <div className="text-black">
-            "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento" 
+            La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento 
         <br>
             Paolo Bosca / Mabo 1971
         <br>
-        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento
         <br>
        Angelo Bruni / Bruni Lab
         <br>
-        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento
        Andrea Perin / Arteba
         <br>
-        " La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento
       Roberta Bonomi / Innovative Surface
         
         <div/>
