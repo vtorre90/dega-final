c505a4ad37f69a1f9e28a1e25f44ff262bc1c99b
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-12 17:22:47 +0200

    Update Contentreviews.js

--- src/components/Contentreviews.js
+++ src/components/Contentreviews.js
@@ -3,19 +3,7 @@ import React from "react";
 function Contentreviews(){
     return(
         <div className="text-black">
-            "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento" 
-        <br>
-            <span class="text-light">Paolo Bosca / Mabo 1971</span>
-        <br>
-        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
-        <br>
-        <span class="text-light">Angelo Bruni / Bruni Lab</span>
-        <br>
-        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
-        <span class="text-light">Andrea Perin / Arteba</span>
-        <br>
-        " La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
-        <span class="text-light">Roberta Bonomi / Innovative Surface</span>
+           ciao
         </div>
     );
 }
