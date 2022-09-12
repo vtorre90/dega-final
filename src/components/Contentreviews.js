4ca135f82c68bde107528bcb04a09622cce9629e
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-12 17:08:18 +0200

    Update Contentreviews.js

--- src/components/Contentreviews.js
+++ src/components/Contentreviews.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function Contentreviews(){
     return(
-        <div className="text-black">
-            La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento" Paolo Bosca / Mabo 1970
+        <div className="text-black text-center">
+            "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento" 
+        <br>
+            <span class="text-light">Paolo Bosca / Mabo 1970</span>
+        <br>
+        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        <br>
+        <span class="text-light">Angelo Bruni / Bruni Lab</span>
+        <br>
+        "La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        <span class="text-light">Andrea Perin / Arteba</span>
+        <br>
+        " La collaborazione con Enrico Deganello ci ha permesso di evolvere come azienda mostrandoci al pubblico come desideravamo e trovando il nostro target di riferimento"
+        <span class="text-light">Roberta Bonomi / Innovative Surface</span>
+
         </div>
     );
 }
