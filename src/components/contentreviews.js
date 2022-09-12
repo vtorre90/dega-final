b54138fe4a3c3e266c911909d78ff554dfa75cd1
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-12 17:00:22 +0200

    Create contentreviews.js

---
+++ src/components/contentreviews.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contentreviews(){
+    return(
+        <div className="text-black">
+            La collaborazione con Enrico Deganello ci ha
+permesso di evolvere come azienda mostrandoci
+al pubblico come desideravamo e trovando il
+nostro target di riferimento"
+Paolo Bosca / Mabo 1970
+        </div>
+    );
+}
+
+export default Contentreviews;
