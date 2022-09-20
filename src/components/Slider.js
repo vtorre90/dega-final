e334abb971438366b939a139f7320ebe4600ab27
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-20 22:52:23 +0200

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -68,6 +68,13 @@ export default function Slider() {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       image: "/assets/slider-6.jpg",
     },
+    {
+      title: "Bruni",
+      title1: "Laboratorio di Falegnameria",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      image: "/assets/slider-6.jpg",
+    },
   ];
 
   const CustomModel = () => (
