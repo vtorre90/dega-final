55eb7472efe2690af36234dabc394f2a85ff54b7
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-09-20 23:28:31 +0200

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -80,7 +80,7 @@ export default function Slider() {
       title1: "Laboratorio di Falegnameria",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      image: "/assets/COVER IMAGE8.jpg",
+      image: "/assets/coverimage8.jpg",
     },
   ];
 
