6313fc1384cadde80f81bad69bc606e8c53b93c4
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-10-16 22:55:18 +0200

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -161,7 +161,8 @@ export default function Slider() {
               <img src="/assets/inn_32.jpg" id="picdesignercard" className="mb-3" />
               <img src="/assets/inn_33.jpg" id="picdesignercard" className="mb-3" />
                 
-               <h4 className="mt-4" >POSTCARDS ADV IS</h4>
+               <h4 className="mt-5" >POSTCARDS ADV IS</h4>
+\\\\\\\\\\\\\\<img src="/assets/inn_34.jpg" id="picdesignercard" className="mb-3" />
             </div>
           )}
         </div>
