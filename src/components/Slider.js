1726ee88bdee402de12d7f59e7087f9f69ff3a20
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-10-17 22:33:52 +0200

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -117,8 +117,8 @@ export default function Slider() {
             className="model-img"
           />
           <div>
-            <h3 className="card-category mt-3">{modelData[modelNum].title}</h3>
-            <h5 className="card-title">{modelData[modelNum].title1}</h5>
+            <h3 className="card-category-int mt-3">{modelData[modelNum].title}</h3>
+            <h5 className="card-title-int">{modelData[modelNum].title1}</h5>
             <p style={{ fontSize: "20px" }}>
               {modelData[modelNum].description}
             </p>
