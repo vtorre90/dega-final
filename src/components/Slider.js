6add746a4cc036e38c2b09e55a149b8ab32bb6de
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-12-03 12:48:05 +0100

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -28,7 +28,7 @@ export default function Slider() {
 
   const modelData = [
     {
-      title: "Bruni",
+      title: "Bruniii",
       title1: "Laboratorio di Falegnameria",
       title3: "2022",
       description:
