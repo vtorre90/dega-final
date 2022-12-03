dba738e7e99a45482f51a037c54add84f565662c
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-12-03 12:50:47 +0100

    Update Covertemplatecontact.js

--- src/components/Covertemplatecontact.js
+++ src/components/Covertemplatecontact.js
@@ -30,8 +30,8 @@ function Convertemplatecontact() {
         <span className="subtitle">Sede legale</span>
         <span>Enrico Deganello</span>
         <span>Via Tiziano Vecellio 73</span>
-        <span>35100, Padova, Italia</span>
-        <span>0000000000</span>
+        <span>35132, Padova, Italia</span>
+        <span>04867810287</span>
       </div>
       <Breadcrumb text="Back to top" />
     </div>
