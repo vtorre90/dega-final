223ef33336adff0c16addb3a4a8c2fae75926309
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-12-17 20:12:02 +0100

    Update ContactPageRefs.js

--- src/components/ContactPageRefs.js
+++ src/components/ContactPageRefs.js
@@ -11,17 +11,13 @@ const ContactPageRefs = () => {
       />
       <div className="text-black">
               <h4>Referenze</h4>
-        <p className="pt-2" style={{ fontWeight: "600" }}>
+        <p>
           "La collaborazione con Enrico Deganello ci ha permesso di evolvere
           come azienda mostrandoci al pubblico come desideravamo e trovando il
           nostro target di riferimento"
         </p>
-        <p>Palo Bosca / Mabo 1970</p>
-        <p className="pt-2" style={{ fontWeight: "600" }}>
-          "La nostra azienda collabora con lo studio Deganello oramai da alcuni anni, come Art direction aziendale. Ritengo che Enrico e il suo team abbia portato in azienda quella marcia in piu’ che mancava. Ho apprezzato nello specifico la grande attenzione che prestano alle piu concrete esigenze aziendali . La parola d’ordine è sempre stata step by step sin dal primo approccio, piccoli passi ma incisivi , volti a portare risultati sia sotto l’aspetto stilistico che commerciale , amplificando i valori primari di un’azienda artigiana spesso lasciati in secondo piano. La comprovata esperienza nel settore del design e dell’architettura fa si che il lavoro svolto risulti sempre mirato all’obbiettivo preposto, cercando sempre il giusto compromesso tra investimenti e profitti. Il focus di ogni progetto è sempre stato quello di far crescere un’azienda con massima attenzione alla tutela della stessa. Un Grazie allo Studio Deganello per ciò che è stato fatto e quello faremo insieme."
-        </p>
-        <p>Angelo Bruni / Titolare Bruni</p>
-        <p className="pt-2" style={{ fontWeight: "600" }}>
+        <p className="pt-2" style={{ fontWeight: "600" }}>Angelo Bruni / Titolare Bruni</p>
+        <p>
           "Collaborando con Enrico ho avuto modo di confrontarmi con una persona molto analitica e concreta. Ai miei 1.000 quesiti, mai una risposta di circostanza, ma sempre discussioni argomentate e soprattutto trasversale, mai banale. Questo modo di confrontarsi ha permesso all’azienda di scavare in profondità nelle dinamiche aziendali facendoci trovare soluzioni che hanno poi avuto un riscontro molto positivo nel mercato. Tra le varie caratteristiche, la capacità di analizzare e sintetizzare i fatti per poi trasformarli in soluzioni aziendali è certamente la qualità che più si è dimostrata più proficua"
         </p>
         <p>Andrea Perin / Titolare Arteba</p>
