b05169b37b5b7f0a938362dc469de014c35a34b9
Veronica Torresin <87823112+vtorre90@users.noreply.github.com>
2022-12-17 20:21:52 +0100

    Update ContactPageRefs.js

--- src/components/ContactPageRefs.js
+++ src/components/ContactPageRefs.js
@@ -21,16 +21,16 @@ const ContactPageRefs = () => {
 <p>
           "Collaborando con Enrico ho avuto modo di confrontarmi con una persona molto analitica e concreta. Ai miei 1.000 quesiti, mai una risposta di circostanza, ma sempre discussioni argomentate e soprattutto trasversale, mai banale. Questo modo di confrontarsi ha permesso all’azienda di scavare in profondità nelle dinamiche aziendali facendoci trovare soluzioni che hanno poi avuto un riscontro molto positivo nel mercato. Tra le varie caratteristiche, la capacità di analizzare e sintetizzare i fatti per poi trasformarli in soluzioni aziendali è certamente la qualità che più si è dimostrata più proficua"
         </p>
-        <p>Andrea Perin / Titolare Arteba</p>
-        <p className="pt-2" style={{ fontWeight: "600" }}>
+        <p className="pt-1 pb-2" style={{ fontWeight: "600" }}>Andrea Perin / Titolare Arteba</p>
+        <p>
           "Lo studio Deganello ha contribuito a dare una organizzazione stilistica e commerciale alla mia azienda gestendo lo sviluppo del materiale grafico in funzione del target di riferimento. Il risultato è stato un aumento di valore della nostra produzione, un messaggio commerciale efficace, un catalogo al top nel suo settore ed una vision a lungo termine. Confrontarsi con una persona di esperienza come Enrico è sempre un piacere poichè sa trasformare domande aziendali in azioni concrete."
         </p>
-        <p>Roberta Bonomi / Innovative Surface</p>
+        <p className="pt-1 pb-2" style={{ fontWeight: "600" }}>Roberta Bonomi / Innovative Surface</p>
  <br>
-<p className="pt-2" style={{ fontWeight: "600" }}>
+<p>
           "Sicuro nell’obiettivo, flessibile nel processo. Direi che questa è la miglior definizione di uno studio unico nel suo genere come quello che Enrico Deganello ha creato. Ho avuto la fortuna di collaborare con Enrico per molti anni e con aziende diverse toccando con mano I risultati ottenuti. Lo studio sa unire e coordinare in modo perfettamente organico ricercatezza stilistica, sviluppo prodotto e azioni commerciali. Risultato garantito."
         </p>
-        <p>Giovanni Defendi  / Consulente Materiali per Studi di Architettura</p>
+        <p className="pt-1 pb-2" style={{ fontWeight: "600" }}>Giovanni Defendi  / Consulente Materiali per Studi di Architettura</p>
       </div>
     </div>
   );
